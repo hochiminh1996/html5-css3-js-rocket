3dafa24d8c80e0e153e62083cedefcd7829335d7
Felippe Marques <ambrsp@hotmail.com>
2023-02-11 16:16:04 -0300

    Introdução ao Javascript

---
+++ Modulo JS/Modulo 01 - Tipos de Dados/00 - Tipos/arquivo.js
@@ -0,0 +1,21 @@
+  /*
+
+    Em JavaScript, existem seis tipos de dados primitivos:
+
+    Number: para números inteiros ou decimais.
+    
+    String: para cadeias de caracteres, ou seja, texto.
+    
+    Boolean: para valores lógicos verdadeiro ou falso.
+    
+    Null: para representar ausência de valor.
+    
+    Undefined: para indicar que uma variável foi declarada mas ainda não possui um valor atribuído.
+    
+    Symbol (novo em ECMAScript 6): para criar valores únicos que não possam ser alterados.
+    
+    
+    Estes tipos de dados são considerados "primitivos" porque são básicos e não podem ser divididos em outros tipos de dados mais simples.
+
+*/
+
---
+++ Modulo JS/Modulo 01 - Tipos de Dados/01 - String/arquivo.js
@@ -0,0 +1,12 @@
+/*
+    Tipo de dados String : uma cadeia de caracteres
+
+    Use aspas simples || duplas
+    Use template String ${}
+
+*/
+
+let nome = "Felippe";
+console.log("Olá, mundo");
+console.log('Olá, mundo');
+console.log(`Olá, ${nome}`);
