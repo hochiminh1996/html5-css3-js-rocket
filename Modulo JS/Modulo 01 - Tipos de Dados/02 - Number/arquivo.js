788c5f62fe7200700969d8ff092d074ffb7c7e0d
Felippe Marques <ambrsp@hotmail.com>
2023-02-11 16:22:57 -0300

    Adicionado o tipo number

---
+++ Modulo JS/Modulo 01 - Tipos de Dados/02 - Number/arquivo.js
@@ -0,0 +1,18 @@
+/*
+    Tipo de dados Number : representa valores inteiros ou decimais(reais)
+
+
+    Dentro do tipo Number temos também : 
+    NAN -> NOT A NUMBER E INFINITY 
+
+*/
+
+let n = 10; // inteiro
+let y = 10.5;// reais ou decimais
+
+let div = n / "a"; // retorna um NAN
+
+
+console.log(n);
+console.log(y);
+console.log(div);
