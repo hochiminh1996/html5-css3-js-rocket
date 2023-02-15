7abbf779fe169ce56190b0b723d9e4b86c125902
Felippe Marques <ambrsp@hotmail.com>
2023-02-15 19:24:42 -0300

    Funções : bloco de código e reutilizando

---
+++ Modulo JS/Modulo 04 - Funções/Aula 00 - Conceitos/arquivo.js
@@ -0,0 +1,14 @@
+/*
+    Função é uma estrutura de dados que armazena um bloco de códigos.
+    É muito útil para realizar tarefas que são repetidas ou não. Além disso, é muito útil para organizar, reutilizar o código
+*/
+
+
+
+function soma(n1, n2) {
+    return n1 + n2;//definindo o bloco de código e suas operações
+}
+
+
+console.log(`Chamando a função e exibindo o retorno do resultado : ${soma(10, 10)}`);
+//chamando a função e passando os parâmetros necessários
