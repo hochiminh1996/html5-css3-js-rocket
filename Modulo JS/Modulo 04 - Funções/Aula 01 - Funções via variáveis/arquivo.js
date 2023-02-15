6b2bfe006c42f722ed81a2ea109b553767f9394f
Felippe Marques <ambrsp@hotmail.com>
2023-02-15 19:34:53 -0300

    Função anônima

--- Modulo JS/Modulo 04 - Funções/Aula 01 - Funções via variáveis/arquivo.js
+++ Modulo JS/Modulo 04 - Funções/Aula 01 - Funções via variáveis/arquivo.js
@@ -4,10 +4,17 @@
 
 
 
-// definindo uma variável constante que armazenará um function. Na function temos os parâmetros passados.
+// definindo uma variável constante que armazenará um function. Na function temos os parâmetros. Parâmetros são n1 e n2
 
 const sum = function (n1, n2) {
     return n1 + n2;
 }
 
+
+
+//chamando a função e passando argumentos. Argumentos são 10 e 15. 
 console.log(sum(10, 15));
+
+
+// Passando valores é argumentado, a exemplo do 10 e 15. Dentro da função, as variáveis n1 e n2, que armazenará os argumentos, serão classificadas como parâmetros.
+
