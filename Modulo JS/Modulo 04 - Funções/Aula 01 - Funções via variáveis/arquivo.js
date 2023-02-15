2d70f8af388dd761372402d176f25ef6ad32c4ae
Felippe Marques <ambrsp@hotmail.com>
2023-02-15 19:31:43 -0300

    Função anônima

---
+++ Modulo JS/Modulo 04 - Funções/Aula 01 - Funções via variáveis/arquivo.js
@@ -0,0 +1,13 @@
+
+
+// usando uma variável para armazenar uma função. Também conhecido como FUNÇÃO ANÔNIMA, já que a function não tem nome, apenas uma referência em memória (via variável  )
+
+
+
+// definindo uma variável constante que armazenará um function. Na function temos os parâmetros passados.
+
+const sum = function (n1, n2) {
+    return n1 + n2;
+}
+
+console.log(sum(10, 15));
