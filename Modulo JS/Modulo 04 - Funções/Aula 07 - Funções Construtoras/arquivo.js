b68dd446ead1494408a038b4d9b3353be8da3cf2
Felippe Marques <ambrsp@hotmail.com>
2023-02-16 00:30:58 -0300

    Modificações no construtor (exemplo)

--- Modulo JS/Modulo 04 - Funções/Aula 07 - Funções Construtoras/arquivo.js
+++ Modulo JS/Modulo 04 - Funções/Aula 07 - Funções Construtoras/arquivo.js
@@ -6,20 +6,20 @@
     * this palavra chave
 */
 
-function Person(nome) {//observe a letra maiúscula em Person (boa prática). Ele será um modelo para nosso objeto
+function Person(nome, idade) {//observe a letra maiúscula em Person (boa prática). Ele será um modelo para nosso objeto
     this.name = nome;//atributo
-
+    this.idade = idade;
 
     // seria nosso método
     this.andar = function () {
-        return `${this.name} está andando`
+        return `${this.name}, com ${this.idade} anos está andando`
     }
 
     // USE sempre o this. Ele indica a referência de cada objeto. Podendo ter N objetos do mesmo tipo
 }
 
 
-const felippe = new Person("Felippe");//criando um objeto com base no construtor person
+const felippe = new Person("Felippe", 25);//criando um objeto com base no construtor person
 const pedro = new Person("Pedro")
 
 
