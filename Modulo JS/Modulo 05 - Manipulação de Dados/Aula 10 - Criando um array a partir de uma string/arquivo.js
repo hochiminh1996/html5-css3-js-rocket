4e3c0fab73d343b101d04f9727952cef849f8f17
Felippe Marques <ambrsp@hotmail.com>
2023-02-18 19:35:33 -0300

    Transformando uma string em array : Array.from(palavra)

---
+++ Modulo JS/Modulo 05 - Manipulação de Dados/Aula 10 - Criando um array a partir de uma string/arquivo.js
@@ -0,0 +1,13 @@
+/*
+
+    Criando um array com base em uma string. Ou seja, cada letra será um elemento do array.
+
+
+*/
+
+let palavra = "Computação";
+
+let elementos = Array.from(palavra);//Fazendo um array da variável palavra
+
+console.log(elementos); // "C", "o","m","p","u","t","a","ç","ã","o"
+console.log(elementos[7])// => ç
