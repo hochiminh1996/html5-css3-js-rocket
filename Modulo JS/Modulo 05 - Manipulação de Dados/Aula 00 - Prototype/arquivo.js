971ea68a98da5b7e7a67e92f12bc9d615a84c7fe
Felippe Marques <ambrsp@hotmail.com>
2023-02-18 15:56:39 -0300

    Tratando dados

---
+++ Modulo JS/Modulo 05 - Manipulação de Dados/Aula 00 - Prototype/arquivo.js
@@ -0,0 +1,13 @@
+/*
+    JS é uma linguagem baseada em protótipo.
+
+
+    Quando criamos um objeto vazio, ele está herdando métodos e atributos do objeto superior. No caso, o obj proto
+
+     "felippe".__proto__
+     Irá nos trazer das as opções de prototipação do objeto superior
+
+ */
+
+
+   
---
+++ Modulo JS/Modulo 05 - Manipulação de Dados/Aula 01 - Type Conversion/arquivo.js
@@ -0,0 +1,6 @@
+/*
+    Type Conversion (typecasting) vs Type Coersion
+ 
+    Alteração de um tipo de dado para outro tipo
+
+ */
