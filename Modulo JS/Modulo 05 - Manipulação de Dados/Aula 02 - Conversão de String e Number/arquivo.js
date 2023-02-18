b6d318ee22c49eb8b06102a3cca31c3d8813766c
Felippe Marques <ambrsp@hotmail.com>
2023-02-18 16:13:25 -0300

    Conversão : Number() e String()

---
+++ Modulo JS/Modulo 05 - Manipulação de Dados/Aula 02 - Conversão de String e Number/arquivo.js
@@ -0,0 +1,10 @@
+/*
+    Manipulando string e números
+
+    Transformando String em número e número em string
+ */
+
+let n="10", y = 11;
+
+console.log(Number(n) + 2); // 12
+console.log(String(y) + 2); // 112
