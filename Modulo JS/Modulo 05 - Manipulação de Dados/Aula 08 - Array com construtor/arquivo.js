66ffdd4f0cb19af38b280cf407561e1d2f91d9dd
Felippe Marques <ambrsp@hotmail.com>
2023-02-18 19:09:50 -0300

    Contando elementos

---
+++ Modulo JS/Modulo 05 - Manipulação de Dados/Aula 08 - Array com construtor/arquivo.js
@@ -0,0 +1,17 @@
+/*
+    Array com construtor
+
+*/
+
+
+let array = ["A","B","C"];// modo de criação padrão de array
+
+let array1 = new Array("A","B","C");//  criando um array usando o construtor
+
+let array2 = new Array(10); // array com  10 posições vazias
+
+console.log(array);
+
+console.log(array1);
+
+console.log(array2);
---
+++ Modulo JS/Modulo 05 - Manipulação de Dados/Aula 09 - Contando Arrays/arquivo.js
@@ -0,0 +1,12 @@
+/*
+
+    Contanto elementos de um array
+
+
+*/
+
+
+// observe que cada elemento pode ter um tipo de dado diferente, incluindo objs e funções
+console.log(["a", {type : "Array"}, "C"].length); //3
+
+
