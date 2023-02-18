a3cec5670af2a8f1a8547f91b00357c691c66ee0
Felippe Marques <ambrsp@hotmail.com>
2023-02-18 16:06:15 -0300

    Type conversion x coersion

--- Modulo JS/Modulo 05 - Manipulação de Dados/Aula 01 - Type Conversion/arquivo.js
+++ Modulo JS/Modulo 05 - Manipulação de Dados/Aula 01 - Type Conversion/arquivo.js
@@ -3,4 +3,12 @@
  
     Alteração de um tipo de dado para outro tipo
 
+    Type Coersion - > O próprio JS fazendo a conversão de dados
+    Type Conversion -> Nós realizando a modificação do tipo de dado
+
  */
+
+
+
+console.log("8"+5);// T. Coersion ->  O JS converte o 5 em string e concatena 
+console.log(Number("8")+5); // T. Conversion -> Eu manualmente faço a conversão do dado para number. 
