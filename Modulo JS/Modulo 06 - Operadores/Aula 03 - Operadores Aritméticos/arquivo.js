06985b2aa40bde1bcad8f0145dc178ff570b2c2e
Felippe Marques <ambrsp@hotmail.com>
2023-02-19 18:15:00 -0300

    Operadores de comparação

---
+++ Modulo JS/Modulo 06 - Operadores/Aula 03 - Operadores Aritméticos/arquivo.js
@@ -0,0 +1,28 @@
+/*
+
+    Operadores Aritméticos
+
+    * MULTIPLICAÇÃO
+    / DIVISAO
+    + SOMA
+    - SUBTRAÇÃO
+
+    % RESTO DA DIVISÃO
+   ++ INCREMENTO
+   -- DECREMENTO
+    ^ EXPONENCIAL
+    
+    
+
+*/
+
+console.log(5 * 5);// Multi
+console.log(5.1/5);// Div
+console.log(1.5 + 2.44); //soma
+console.log(502 - 102); // sub
+console.log(5%2); //modulo
+
+let n = 0;
+console.log(n++);//incremento
+console.log(n--);//decremento
+console.log(2**2);//expo
---
+++ Modulo JS/Modulo 06 - Operadores/Aula 04 - Operadores de comparação/arquivo.js
@@ -0,0 +1,30 @@
+/*
+
+    Operadores de comparação
+
+*/
+
+
+
+
+let one = 1;
+let two = 2;
+
+console.log(one == 1);//true
+console.log(one == "1");//true
+console.log(one === "1"); //false. Comparação e o tipo de dado
+
+console.log(one!=two);// true
+console.log(one!=1); //false
+console.log(two!=2);// false
+
+
+
+//operadores estritamento igual ou diferente
+
+console.log(one != "1");//false. Pois ele considera que são iguais, mesmo o tipo sendo diferente
+
+console.log(one!=1); // false. Pois são iguais
+
+console.log(one !=="1");// true. Porque ele considera o tipo de dado
+
