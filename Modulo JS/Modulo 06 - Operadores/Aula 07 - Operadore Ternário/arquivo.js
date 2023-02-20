f6921ff93548b16f6c4b0588e535b6f281c0b71a
Felippe Marques <ambrsp@hotmail.com>
2023-02-19 21:52:07 -0300

    Ternário

--- Modulo JS/Modulo 06 - Operadores/Aula 07 - Operadore Ternário/arquivo.js
+++ Modulo JS/Modulo 06 - Operadores/Aula 07 - Operadore Ternário/arquivo.js
@@ -5,4 +5,10 @@
 
 */
 
-let idade = 25;
+let idade = 12, bread = true, milk = true;
+
+
+console.log(idade >= 18 ? "Maior de idade" : "Menor de Idade");
+
+console.log(bread && milk ? "Café da manhã básico" : "Café da manhã ruim");
+console.log(bread || milk ? "Café da manhã básico" : "Café da manhã ruim");
