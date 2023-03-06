4549aaa31be1946907041366f0f24281f0b71a83
hochiminh1996 <ambrsp@hotmail.com>
2023-03-06 17:21:14 -0300

    getElementsByClassName

---
+++ Modulo JS  - DOM/01 - Selecionando Elementos/02 - GetElementsByClassName/arquivo.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", main);
+
+
+// pegando seletores do tipo classe
+function main() {
+    const titulo = document.getElementsByClassName("titulo");
+    //  a busca por seletor class, que possui vários, retorna um coleção de html : [n é um array]
+
+    for (let i in titulo) {
+        
+        if (titulo[i].textContent != undefined) {//verifica se não retorna um undefined, já que se trata de uma coleção html
+            alert(titulo[i].textContent)
+        }
+        
+    }
+
+
+}
+
+
+
+
