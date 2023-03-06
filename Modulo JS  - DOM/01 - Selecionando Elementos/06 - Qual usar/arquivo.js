f331e39a70fbb0d6426231cb36a3e90abfc1c0f6
hochiminh1996 <ambrsp@hotmail.com>
2023-03-06 18:22:50 -0300

    Atualizações

---
+++ Modulo JS  - DOM/01 - Selecionando Elementos/06 - Qual usar/arquivo.js
@@ -0,0 +1,16 @@
+/*
+    Qual usar?
+
+    getElementById -> pega um elemento de ID único (id)
+
+    getElementsByClassName -> retorna uma coleção de elem. de html. O foreach n funciona com a coleção de elem. html
+
+    getElementsByTagName -> retorna tb uma coleção de html
+
+    querySelector : busca qualquer elemento do tipo: classe, id, pseudoelemento, tag e etc
+
+    querySelectorAll : retorna um nodelist que é possível percorrer com um foreach.
+    
+
+
+*/
