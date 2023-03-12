c9384d1a9b243c2389fe3cd03fa33e56cda6c5de
hochiminh1996 <ambrsp@hotmail.com>
2023-03-12 16:45:48 -0300

    Eventos e o parâmetro event

---
+++ Modulo JS  - DOM/05 - Eventos/03 - Argumento Event/arquivo.js
@@ -0,0 +1,9 @@
+
+
+const input = document.querySelector("input#label-1");
+
+
+input.onclick = function(event){
+    console.log(event);
+}
+// o parâmetro evento contém uma série de informações sobre quem chamou o evento ou qual seu tipo
