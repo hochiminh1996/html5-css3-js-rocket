6af72d6127630dc9aa56877aa6446cecaf426ea8
hochiminh1996 <ambrsp@hotmail.com>
2023-03-12 15:51:14 -0300

    Eventos

---
+++ Modulo JS  - DOM/05 - Eventos/01 - Adicionando um Evento/arquivo.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", main);
+// adicionando um evento ao carregar o DOM.
+
+
+
+function main() {
+    const main = document.querySelector("#principal");
+
+    const h1 = document.createElement("h1");
+    const h2 = document.createElement("h2");
+    const input = document.createElement("input");
+    const btn = document.createElement("input");
+
+
+
+
+    h1.setAttribute("id", "titulo-01");
+    h1.textContent = "Olá, mundo";
+    h2.textContent = "Título N2";
+    input.placeholder = "Digite um valor";
+    input.style.padding = "10px";
+    input.style.border = "1px solid #d9d9d9";
+    input.style.borderRadius = "5px";
+    input.style.outline = "none";
+    btn.type = "button";
+    btn.value = "Botão";
+    btn.style.padding = "10px";
+    btn.style.border = "1px solid #d9d9d9";
+    btn.style.borderRadius = "5px";
+    btn.style.cursor = "pointer";
+
+    main.append(h1);//adicionando um elemento
+    main.insertBefore(h2, h1.nextSibling);//adicionando um elemento após o h1
+
+    main.appendChild(input);
+    main.appendChild(btn);
+
+    // adicionando um evento do tipo de mouse 
+    btn.addEventListener("mouseover", () => {
+        eventos();
+    })
+
+  
+
+   
+
+
+}
+
+function eventos() {
+    alert("Olá, amigo. Você ativou um evento");
+}
